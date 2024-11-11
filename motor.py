27feb06a39e6887875f03d8bb54b64259eccb26a
Nolan <nolan.lem@gmail.com>
2024-11-11 19:08:08 +0100

    tested stepper motor code

--- motor.py
+++ motor.py
@@ -4,8 +4,8 @@ import board
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit(i2c=board.I2C()) 
-kit.motor1.throttle = 0.5 # run motor at half speed 
+kit.motor3.throttle = 0.5 # run motor at half speed 
 time.sleep(2.0) # wait 2 seconds 
-kit.motor1.throttle = 0 # turn off 
+kit.motor3.throttle = 0 # turn off 
 
 #%%
--- stepper-motor.py
+++ stepper-motor.py
@@ -2,9 +2,11 @@
 import time
 import board
 from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper 
 
 kit = MotorKit(i2c=board.I2C())
 
 for i in range(100):
-    kit.stepper2.onestep()
-    time.sleep(0.01)
+	kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+	#kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+	time.sleep(0.01)
